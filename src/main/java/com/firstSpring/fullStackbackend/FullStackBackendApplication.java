34168a695343e9f89071be348ebdef0a73217fcc
KRChapman <splash004@gmail.com>
2023-01-20 16:03:23 -0800

    setup env

--- src/main/java/com/firstSpring/fullStackbackend/FullStackBackendApplication.java
+++ src/main/java/com/firstSpring/fullStackbackend/FullStackBackendApplication.java
@@ -1,13 +1,36 @@
 package com.firstSpring.fullStackbackend;
 
+import lombok.extern.log4j.Log4j2;
+//import org.hibernate.cfg.Environment;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
+@Log4j2
 @SpringBootApplication
 public class FullStackBackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FullStackBackendApplication.class, args);
+	}
+
+
+
+	@Bean
+	ApplicationRunner applicationRunner(Environment environment){
+		System.out.println("hi78687");
+		return new ApplicationRunner() {
+			@Override
+			public void run(ApplicationArguments args) throws Exception {
+	  			log.info("hiaaaa"+ environment.getProperty("API_KEY"));
+				System.out.println("hi");
+
+			}
+		};
 	}
 
 }
--- src/main/java/com/firstSpring/fullStackbackend/repository/UserRepository.java
+++ src/main/java/com/firstSpring/fullStackbackend/repository/UserRepository.java
@@ -1,4 +1,7 @@
-package com.firstSpring.fullStackbackend.repository;
+//package com.firstSpring.fullStackbackend.repository;
+//
+//import com.firstSpring.fullStackbackend.model.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
 
-public class UserRepository {
-}
+//public interface UserRepository extends JpaRepository<User,Long> {
+//}
